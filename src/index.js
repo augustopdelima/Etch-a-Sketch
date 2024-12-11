38aade04b1cc335c46e95f2b3f434da03ddd59ed
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-12-11 16:02:35 -0300

    Add functionality to dynamically adjust grid size

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
 function addHover() {
     const grid = document.getElementById("root");
-    grid.addEventListener("mouseover", (event => {
+    grid.addEventListener("mouseover", (event) => {
 
         console.log(event.target.tagName);
 
-        if (event.target.tagName != "DIV"){
+        if (event.target.tagName != "DIV") {
             return;
         }
-        
+
         const block = document.getElementById(event.target.id);
         block.style.background = "black";
-    }));
+    });
 }
 
 
@@ -30,5 +30,29 @@ function createGrid(gridBlocks = 16) {
     }
 }
 
-createGrid(16);
+function removeGrid(){
+    const grid = document.getElementById("root");
+    let child = grid.lastElementChild;
+    while(child){
+        grid.removeChild(child);
+        child = grid.lastElementChild;
+    }
+}
+
+function handleChangeGridDisplay() {
+    const input = document.getElementById("input-grid");
+
+    input.addEventListener("change", (event) => {
+        let value = Number(event.target.value);
+
+        if(value < 1) return;
+        if(value > 100) return;
+
+        removeGrid();
+        createGrid(value);
+    });
+}
+
+createGrid();
 addHover();
+handleChangeGridDisplay();
