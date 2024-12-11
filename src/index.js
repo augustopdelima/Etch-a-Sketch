816ead961214c22f44742b6e32ac58cb71987b0d
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-12-11 13:46:18 -0300

    Add change color when mouse hover

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-const grid = document.getElementById("root");
-let gridBlocks = 16;
-let gridArea = gridBlocks * gridBlocks;
-let blockSize = grid.clientWidth / gridBlocks - 2;
 
-for (let i = 0; i < gridArea; i++) {
-    let block = document.createElement("div");
-    block.classList.add("grid-block"); 
-    block.style.width = `${blockSize}px`
-    
-    grid.appendChild(block);
+function addHover() {
+    const grid = document.getElementById("root");
+    grid.addEventListener("mouseover", (event => {
+
+        console.log(event.target.tagName);
+
+        if (event.target.tagName != "DIV"){
+            return;
+        }
+        
+        const block = document.getElementById(event.target.id);
+        block.style.background = "black";
+    }));
 }
+
+
+function createGrid(gridBlocks = 16) {
+    const grid = document.getElementById("root");
+    let gridArea = gridBlocks * gridBlocks;
+    let blockSize = grid.clientWidth / gridBlocks - 2;
+
+    for (let i = 0; i < gridArea; i++) {
+        let block = document.createElement("div");
+        block.id = i;
+        block.classList.add("grid-block");
+        block.style.width = `${blockSize}px`
+
+        grid.appendChild(block);
+    }
+}
+
+createGrid(16);
+addHover();
